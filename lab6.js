df3dc5255903dd0c45f9c6862cd1a6221c4d05f5
ayah <breakayah92@gmail.com>
2021-02-23 16:20:33 +0200

    new asac web site

---
+++ lab6.js
@@ -0,0 +1,16 @@
+var age =18;
+var ageUserInput=prompt("write your age");
+
+if (age==ageUserInput){
+    alert("blessing");
+    console.log("khedmat watan");
+}
+else{
+    alert("sorry");
+    console.log("software develober");
+
+}
+
+
+
+
