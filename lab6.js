ebb704683f6c8bd892db6797557103b44aa006b1
ayah <breakayah92@gmail.com>
2021-02-24 16:58:20 +0200

    add while to code

--- lab6.js
+++ lab6.js
@@ -1,16 +1,33 @@
-var age =18;
-var ageUserInput=prompt("write your age");
+//var age =18;
+//var ageUserInput=prompt("write your age");
 
-if (age==ageUserInput){
-    alert("blessing");
-    console.log("khedmat watan");
+//if (age==ageUserInput){
+    //alert("blessing");
+  //console.log("khedmat watan")
+//}
+//else{
+  //  alert("sorry");
+    // console.log("software develober");
+
+//}
+
+// document.getElementById("demo");
+//   var car("mersedes","van","hundai");
+//   var UserInput.prompt("guess the car");
+//  while(car !=UserInput){
+//    alert("true ");
+  
+// }
+// alert("try again ");
+ 
+ 
+
+//     }
+// }
+
+var UserInput = prompt("what the car is hundai?red or blue or yelow");
+while(UserInput!="red"||UserInput!="blue"||UserInput!="yelow"){
+
+  alert("sorry for you");
+ 
 }
-else{
-    alert("sorry");
-    console.log("software develober");
-
-}
-
-
-
-
