3d544297a6081b31bee2d672f4b3a7ead92687fe
ayah <breakayah92@gmail.com>
2021-02-25 10:35:03 +0200

    add function name

--- project/java-script.js
+++ project/java-script.js
@@ -1,14 +1,22 @@
 var txt;
 var r = confirm("Enjoy Your journey learning!");
 if (r == true) {
-alert("hello");
 var number = prompt("How many GIF you want to see? ");
 for(var i = 0; i < number; i++) {
   document.write('<img src="https://cdn.dribbble.com/users/1059583/screenshots/4171367/coding-freak.gif" alt="">');
- }
+}
+
 
 } else {
   alert("please visit us again");
 }
+var name =prompt("whats your name");
+function printMyName(myName) {
+    alert("happy to meet you  " + myName);
 
-var number = prompt("How many GIF you want to see? ");
+}
+
+printMyName(name);
+
+
+
